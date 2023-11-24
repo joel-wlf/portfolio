ae96457b934efabfc1ca536a5051e988e994cd5e
Joel <72318130+joel-wlf@users.noreply.github.com>
2023-11-24 21:51:41 +0100

    reusable title class

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,13 @@ import FactsCards from "./FactsCards"
 
 function About() {
 
-    const facts = ['Location 🡢 Germany', 'Fact 2', 'Fact 3', 'Fact 4']
+    const facts = ['Fact 1', 'Fact 2', 'Fact 3', 'Fact 4', 'Fact 5']
 
     const factsCards = facts.map(fact => <FactsCards key={fact} text={fact} />)
 
     return (
         <section className="about">
-            <h2 className="about--title">About</h2>
+            <h2 className="section--title">About</h2>
             <div className="about--facts">
                 {factsCards}
             </div>
